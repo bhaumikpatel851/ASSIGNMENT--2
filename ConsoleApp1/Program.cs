ce07f8fa0bec7d17ca77eef53d456e7322776db5
bhaumikpatel851 <134671922+bhaumikpatel851@users.noreply.github.com>
2024-06-08 16:55:16 -0400

    HUNTER GAME PART-1
    This is a 2D game ,I have selected X and Y values in it and there is name, position, Gemcount till over here.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Position
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public Position(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
+
+class Player
+{
+    public string Name { get; set; }
+    public Position Position { get; set; }
+    public int GemCount { get; set; }
+
+    public Player(string name, Position position)
+    {
+        Name = name;
+        Position = position;
+        GemCount = 0;
+    }
+}
