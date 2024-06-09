dffc8c66ec27d606d8787c00121d93fc3fbcc708
bhaumikpatel851 <134671922+bhaumikpatel851@users.noreply.github.com>
2024-06-09 18:49:15 -0400

    HUNTER GAME PART-2
    In this part the game is for moving players in the game.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -24,4 +24,25 @@ class Player
         Position = position;
         GemCount = 0;
     }
+    public void Move(char direction)
+    {
+        // Handle movement logic based on the direction
+        // You can add boundary checks and obstacle checks here
+        switch (direction)
+        {
+            case 'u':
+                Position.Y--;
+                break;
+            case 'd':
+                Position.Y++;
+                break;
+            case 'l':
+                Position.X--;
+                break;
+            case 'r':
+                Position.X++;
+                break;
+        }
+    }
 }
+
