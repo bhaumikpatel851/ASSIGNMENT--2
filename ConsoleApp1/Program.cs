aaaf6f45f1b34ab20bee1904bba0db050573fc3d
bhaumikpatel851 <134671922+bhaumikpatel851@users.noreply.github.com>
2024-06-10 18:00:21 -0400

    HUNTER  GAME PART - 3
    In this part it is about the players that moves from the position and also there some it is little complex but I have tryed my best for it there is cell and grid in this part.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -45,4 +45,55 @@ class Player
         }
     }
 }
+class Cell
+{
+    public string Occupant { get; set; }
 
+    public Cell(string occupant)
+    {
+        Occupant = occupant;
+    }
+}
+
+class Board
+{
+    public Cell[,] Grid { get; set; }
+
+    public Board()
+    {
+        // Initialize the board with empty cells
+        Grid = new Cell[6, 6];
+        for (int i = 0; i < 6; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                Grid[i, j] = new Cell("-");
+            }
+        }
+
+        // Add players, gems, and obstacles
+        // You may want to randomize the positions
+        Grid[0, 0].Occupant = "P1";
+        Grid[5, 5].Occupant = "P2";
+
+        Grid[2, 2].Occupant = "G";
+        Grid[3, 3].Occupant = "G";
+
+        Grid[1, 1].Occupant = "O";
+        Grid[4, 4].Occupant = "O";
+    }
+
+    public void Display()
+    {
+        // Display the current state of the board
+        for (int i = 0; i < 6; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                Console.Write(Grid[i, j].Occupant + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
